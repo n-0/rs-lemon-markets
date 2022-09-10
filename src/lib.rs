b306bb32489c3da96e1ba226f28b52ce8d767fb8
n-0 <n-0@github.com>
2022-09-10 16:41:55 +0200

    feat: initial commit
    Cleaned up autogenerated files and fused two openapi specs
    into one. See more in the [readme](README.md).

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+#[macro_use]
+extern crate serde_derive;
+
+extern crate serde;
+extern crate serde_json;
+extern crate url;
+extern crate reqwest;
+
+pub mod market_data;
+pub mod trading;
+pub mod util;
